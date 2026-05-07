ab82911115e4c82d71bea6a1ecf51b0ddbefbc24
HiorCraft <liam.fanenbruck@gmail.com>
2026-05-07 19:13:32 +0200

    Refactor nexTabCommand to improve command structure and enhance reload functionality

--- src/main/kotlin/dev/hiorcraft/nex/tab/command/nexTabCommand.kt
+++ src/main/kotlin/dev/hiorcraft/nex/tab/command/nexTabCommand.kt
@@ -1,43 +1,33 @@
 package dev.hiorcraft.nex.tab.command
 
+import com.github.shynixn.mccoroutine.folia.launch
 import dev.hiorcraft.nex.tab.plugin
+import dev.hiorcraft.nex.tab.tablistConfigProvider
 import dev.hiorcraft.nex.tab.tablistService
-import dev.hiorcraft.nex.tab.util.PermissionRegistry
 import dev.jorel.commandapi.kotlindsl.anyExecutor
 import dev.jorel.commandapi.kotlindsl.commandTree
 import dev.jorel.commandapi.kotlindsl.literalArgument
 import dev.slne.surf.surfapi.core.api.messages.adventure.sendText
 import org.bukkit.Bukkit
 
+fun nexTabCommand() = commandTree("nexTab") {
+    withPermission("surf.tab.command.surftab")
 
-fun nexTabCommand() = commandTree("nextab") {
-    withAliases("tab")
+    literalArgument("reload") {
+        anyExecutor { executor, _ ->
+            tablistConfigProvider.reload()
 
-    withPermission(PermissionRegistry.COMMAND_TAB)
+            plugin.launch {
+                Bukkit.getOnlinePlayers().forEach {
+                    tablistService.sendAdditions(it)
+                    tablistService.formatPlayer(it)
+                }
+            }
 
-    anyExecutor { executor, _ ->
-
-        executor.sendText{
-            appendInfoPrefix()
-            secondary("Du kannst die Tablist mit /nextab reload neu laden.")
-        }
-    }
-
-    literalArgument("reload")
-
-    anyExecutor { executor, _ ->
-
-        Bukkit.getOnlinePlayers().forEach { player ->
-            player.scheduler.run(plugin, { _ ->
-                tablistService.sendAdditions(player)
-                tablistService.formatPlayer(player)
-            }, null)
-        }
-
-        executor.sendText{
-            appendInfoPrefix()
-            success("Die Tablist wurde neu geladen")
+            executor.sendText {
+                appendSuccessPrefix()
+                success("Die Tablist wurde neu geladen.")
+            }
         }
     }
 }
-
